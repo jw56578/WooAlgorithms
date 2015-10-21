837a67e2af8216133bbf4af37ddaf973c7254adc
jw56578 <jw56578@gmail.com>
2015-10-21 18:00:29 -0500

    trying stuff

---
+++ WooAlgorithms/WooAlgorithms/QuickFind.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WooAlgorithms
+{
+    public class QuickFind
+    {
+        public int[] id;
+
+        public QuickFind(int n)
+        {
+            id = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                id[i] = i;
+            }
+        }
+        public bool Connected(int p, int q)
+        {
+            return id[p] == id[q];
+        }
+        public void Union(int p, int q)
+        {
+            int pid = id[p];
+            int qid = id[q];
+            for (int i = 0; i < id.Length; i++)
+            {
+                if (id[i] == pid) id[i] = qid;
+            }
+        }
+
+    }
+}
---
+++ WooAlgorithms/WooAlgorithms/QuickUnion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WooAlgorithms
+{
+    public class QuickUnion
+    {
+         public int[] id;
+
+         public QuickUnion(int n)
+        {
+            id = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                id[i] = i;
+            }
+        }
+         int Root(int i)
+         {
+             while (i != id[i]) i = id[i];
+             return i;
+         }
+        public bool Connected(int p, int q)
+        {
+            return Root(p) == Root(q);
+        }
+        public void Union(int p, int q)
+        {
+            int i = Root(p);
+            int j = Root(q);
+            id[i] = j;
+        }
+    }
+}
